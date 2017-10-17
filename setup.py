abeb70b687fb148087f83c9dc969c952faa6838f
QijingZheng <zqj.kaka@gmail.com>
2017-10-17 16:13:12 +0800

    add a setup file of distutils

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name            = 'PyVaspWfc',
+    version         = '1.0',
+    description     = 'Python modules for dealing with VASP pseudo-wavefunctions.',
+    author          = 'Qijing Zheng',
+    author_email    = 'zqj.kaka@gmail.com',
+    url             = 'https://github.com/QijingZheng/VaspBandUnfolding',
+    # packages        = ['pyvaspwfc',],
+    py_modules      = ['vasp_constant', 'vaspwfc', 'nac', 'unfold']
+)
