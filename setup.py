ec61f25919452f1fb5c5050a8923391cde2a6dc0
QijingZheng <zqj.kaka@gmail.com>
2020-07-21 21:09:55 +0800

    [bin/wfcplot] add helping script to save wavefunction

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 from distutils.core import setup
 
 setup(
-    name            = 'PyVaspWfc',
-    version         = '1.0',
-    description     = 'Python modules for dealing with VASP pseudo-wavefunctions.',
-    author          = 'Qijing Zheng',
-    author_email    = 'zqj.kaka@gmail.com',
-    url             = 'https://github.com/QijingZheng/VaspBandUnfolding',
+    name         = 'PyVaspWfc',
+    version      = '1.0',
+    description  = 'Python modules for dealing with VASP pseudo-wavefunctions.',
+    author       = 'Qijing Zheng',
+    author_email = 'zqj.kaka@gmail.com',
+    url          = 'https://github.com/QijingZheng/VaspBandUnfolding',
     # packages        = ['pyvaspwfc',],
-    py_modules      = [
+    py_modules   = [
         'vasp_constant', 'vaspwfc', 'nac', 'unfold', 'procar', 'spinorb',
         'sph_harm', 'paw'
-    ]
+    ],
+    scripts      = ['bin/wfcplot']
 )
